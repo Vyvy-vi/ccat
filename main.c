66ed33c1b73acbd96a42705a0bf4580ecf5c4b27
Vy <vyom.j@protonmail.com>
2024-08-23 11:26:37 +0530

    feat: initial version

---
+++ main.c
@@ -0,0 +1,63 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
+#include "stdbool.h"
+
+#define BUFFER_SIZE 100
+
+char buffer[BUFFER_SIZE];
+
+
+struct cmdFlags {
+	bool number;
+	bool blankOmitNumber;
+};
+
+void read_stdin2_stdout(struct cmdFlags flags) {
+	while (1) {
+		size_t cnt = fread(&buffer, sizeof(char), BUFFER_SIZE - 1, stdin);
+		buffer[cnt] = '\0';
+		fwrite(buffer, sizeof(char), cnt, stdout);
+		fflush(stdout);
+		buffer[0] = '\0';
+			
+		if (cnt < BUFFER_SIZE - 1 && feof(stdin)) break;
+	}
+
+}
+
+void readFile2_stdout(char* filename) {
+	FILE* filePtr = fopen(filename, "r");
+
+	while (!feof(filePtr)) {
+		size_t cnt = fread(&buffer, sizeof(char), BUFFER_SIZE - 1, filePtr);
+		buffer[cnt] = '\0';
+		printf("%s", buffer);
+		buffer[0] = '\0';		
+	}
+
+	fclose(filePtr);
+}
+
+
+int main(int argc, char **argv) {
+	if (argc == 1) {
+		read_stdin2_stdout();
+	} else {
+		struct cmdFlags flags = {false, false};
+		int argIndex = 1;
+
+		while (argIndex < argc) {
+			char arg = argv[argIndex];
+			if (arg[0] != '-') break;
+			else if (arg[1] == 'n') cmdFlags.number = true;
+			else if (arg[1] == 'b') cmdFlags.blankOmitNumber = true;
+			argIndex++;
+		}
+		
+		while (argIndex < argc) {
+			readFile2_stdout(argv[argIndex]);
+			argIndex++;
+		}
+	}
+}
