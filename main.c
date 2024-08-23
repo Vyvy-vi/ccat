4bc3cf0df931efbb8254d83962279247c256ccc4
Vy <vyom.j@protonmail.com>
2024-08-23 13:04:12 +0530

    feat: flag support

--- main.c
+++ main.c
@@ -2,48 +2,75 @@
 #include "stdlib.h"
 #include "unistd.h"
 #include "stdbool.h"
+#include "limits.h"
 
 #define BUFFER_SIZE 100
-
-char buffer[BUFFER_SIZE];
-
 
 struct cmdFlags {
 	bool number;
 	bool blankOmitNumber;
 };
 
-void read_stdin2_stdout() {
-	while (1) {
-		size_t cnt = fread(&buffer, sizeof(char), BUFFER_SIZE - 1, stdin);
-		buffer[cnt] = '\0';
-		fwrite(buffer, sizeof(char), cnt, stdout);
-		fflush(stdout);
-		buffer[0] = '\0';
-			
-		if (cnt < BUFFER_SIZE - 1 && feof(stdin)) break;
+void read_stdin2_stdout(struct cmdFlags flags) {
+	char *line = malloc(LINE_MAX);
+	if (line == NULL) {
+		printf("Not enough space...");
+		return;
 	}
 
+	int counter = 0;
+	while (fgets(line, LINE_MAX, stdin) != NULL) {
+		if (flags.blankOmitNumber) {
+			if (line[0] != '\n') {
+				counter++;  
+				printf("%6d\t%s", counter, line);
+			} else {
+				printf("%s", line);
+			}
+		} else if (flags.number) {
+			counter++;
+			printf("%6d\t%s", counter, line);
+		} else {
+			printf("%s", line);
+		}
+		fflush(stdout);
+	}
+	free(line);
 }
 
 void readFile2_stdout(char* filename, struct cmdFlags flags) {
 	FILE* filePtr = fopen(filename, "r");
 
-	while (!feof(filePtr)) {
-		size_t cnt = fread(&buffer, sizeof(char), BUFFER_SIZE - 1, filePtr);
-		buffer[cnt] = '\0';
-		printf("%s", buffer);
-		buffer[0] = '\0';		
+	char *line = malloc(LINE_MAX);
+	if (line == NULL) {
+		printf("Not enough space...");
+		return;
 	}
 
+	int counter = 0;
+	while (fgets(line, LINE_MAX, filePtr) != NULL) {
+		if (flags.blankOmitNumber) {
+			if (line[0] != '\n') {
+				counter++;  
+				printf("%6d\t%s", counter, line);
+			} else {
+				printf("%s", line);
+			}
+		} else if (flags.number) {
+			counter++;
+			printf("%6d\t%s", counter, line);
+		} else {
+			printf("%s", line);
+		}
+	}
+
+	free(line);
 	fclose(filePtr);
 }
 
 
 int main(int argc, char **argv) {
-	if (argc == 1) {
-		read_stdin2_stdout();
-	} else {
+		
 		struct cmdFlags flags = {false, false};
 		int argIndex = 1;
 
@@ -54,10 +81,13 @@ int main(int argc, char **argv) {
 			else if (arg[1] == 'b') flags.blankOmitNumber = true;
 			argIndex++;
 		}
+
+		if (argIndex == argc) {
+			read_stdin2_stdout(flags);
+		}
 		
 		while (argIndex < argc) {
 			readFile2_stdout(argv[argIndex], flags);
 			argIndex++;
 		}
-	}
 }
